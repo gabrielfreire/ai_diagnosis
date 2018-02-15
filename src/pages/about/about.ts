e1d875cd9700e5ece6c7f4187c9bc171fe849155
gabrielfreire <gabriel.freire@imsmaxims.com>
2018-02-15 16:59:51 +0000

    fix

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,93 +7,298 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
   dataie: string = `{
-      "Questions": {
-          "flu": [{
-                  "key": "firstName",
-                  "label": "First name",
-                  "value": "",
-                  "type": "text",
-                  "required": true,
-                  "order": 1,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "lastName",
-                  "label": "Last name",
-                  "value": "",
-                  "type": "text",
-                  "required": false,
-                  "order": 2,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "age",
-                  "label": "Age",
-                  "value": "",
-                  "type": "number",
-                  "required": false,
-                  "order": 3,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "soreThroatLevel",
-                  "label": "Sore Throat [from 1 to 10]",
-                  "value": "",
-                  "type": "number",
-                  "required": false,
-                  "order": 4,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "stuffyNoseLevel",
-                  "label": "Stuffy Nose [from 1 to 10]",
-                  "value": "",
-                  "type": "number",
-                  "required": false,
-                  "order": 5,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "headacheLevel",
-                  "label": "Headache strenght [from 1 to 10]",
-                  "value": "",
-                  "type": "number",
-                  "required": false,
-                  "order": 6,
-                  "visibility": "visible",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "feverLevel",
-                  "label": "How strong is your Fever? [from 1 to 10]",
-                  "value": "",
-                  "type": "number",
-                  "required": false,
-                  "order": 8,
-                  "visibility": "hidden",
-                  "controlType": "textbox"
-              },
-              {
-                  "key": "hasfever",
-                  "label": "Do you feel fever?",
-                  "value": false,
-                  "type": "toggle",
-                  "required": false,
-                  "visibility": "visible",
-                  "order": 7,
-                  "event": {
-                      "target": "feverLevel"
-                  },
-                  "controlType": "toggle"
-              }
-          ]
-      }
-  }`
+    "metadata": null,
+    "questionset": [{
+        "additional_information": {
+            "title": "Patient Health Questionnaire (PHQ-9)",
+            "created_at": null,
+            "updated_at": null,
+            "question_id": null,
+            "subheader": "Validity has been assessed against an independent structured mental health professional (MHP) interview. PHQ-9 score ≥10 had a sensitivity of 88% and a specificity of 88% for major depression. It can even be used over the telephone."
+        },
+        "questions": [{
+                "key": "question1",
+                "label": "Little interest or pleasure in doing things?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 0,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question2",
+                "label": "Feeling down, depressed, or hopeless?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 1,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question3",
+                "label": "Trouble falling or staying asleep, or sleeping too much?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 2,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question4",
+                "label": "Feeling tired or having little energy?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 3,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question5",
+                "label": "Poor appetite or overeating?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 4,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question6",
+                "label": "Feeling bad about yourself - or that you are a failure or have let yourself or your family down?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 5,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question7",
+                "label": "Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 6,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question8",
+                "label": "Thoughts that you would be better off dead, or of hurting yourself in some way?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 7,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            },
+            {
+                "key": "question9",
+                "label": "Trouble concentrating on things, such as reading the newspaper or watching television?",
+                "value": "",
+                "required": true,
+                "options": [{
+                        "key": 0,
+                        "value": "Not at all"
+                    },
+                    {
+                        "key": 1,
+                        "value": "Several days"
+                    },
+                    {
+                        "key": 2,
+                        "value": "More than half the days"
+                    },
+                    {
+                        "key": 3,
+                        "value": "Nearly every day"
+                    }
+                ],
+                "order": 8,
+                "visibility": "visible",
+                "controlType": "dropdown"
+            }
+        ],
+        "evaluation": [{
+                "maxscore": 27,
+                "event": {
+                    "type": "score_id",
+                    "message": "Severe"
+                },
+                "fact": "mental_health_condition",
+                "path": ".score"
+            },
+            {
+                "maxscore": 19,
+                "event": {
+                    "type": "score_id",
+                    "message": "Moderately Severe"
+                },
+                "fact": "mental_health_condition",
+                "path": ".score"
+            },
+            {
+                "maxscore": 14,
+                "event": {
+                    "type": "score_id",
+                    "message": "Moderate"
+                },
+                "fact": "mental_health_condition",
+                "path": ".score"
+            },
+            {
+                "maxscore": 9,
+                "event": {
+                    "type": "score_id",
+                    "message": "Mild"
+                },
+                "fact": "mental_health_condition",
+                "path": ".score"
+            },
+            {
+                "maxscore": 4,
+                "event": {
+                    "type": "score_id",
+                    "message": "None"
+                },
+                "fact": "mental_health_condition",
+                "path": ".score"
+            }
+        ]
+    }]
+}`
   
   ruleie = `{
       conditions: {
