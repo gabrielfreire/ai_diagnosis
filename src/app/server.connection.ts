0e06049c9622c32e4ee9d0ad1a8e89f81bfd4c7b
Gabriel Freire <gabrielfreiredev@gmail.com>
2018-04-26 21:00:02 +0100

    fix vision url

--- src/app/server.connection.ts
+++ src/app/server.connection.ts
@@ -1,5 +1,5 @@
 export const server = {
     // url: 'http://localhost:3000/api'
     url: 'https://imsdiag.herokuapp.com/api',
-    vision_url: 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories%2CDescription%2CColor&details=&language=en'
+    vision_url: 'https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories%2CDescription%2CColor&details=&language=en'
 }
--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,3 +1,3 @@
 export default {
-    VISION_API_KEY: "82d9092e15f0446b8bc4e55e8bf07d0e"
+    VISION_API_KEY: "6c2ca639591f4c00bea957ad371c36ac"
 }
