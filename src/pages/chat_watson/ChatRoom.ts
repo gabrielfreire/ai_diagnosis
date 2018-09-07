2f1e42592e4887ff66a4e66c130af80fa24a6215
gabrielfreire <gabriel.freire@imsmaxims.com>
2018-09-07 13:59:53 +0100

    d

--- src/pages/chat_watson/ChatRoom.ts
+++ src/pages/chat_watson/ChatRoom.ts
@@ -10,6 +10,14 @@ export class ChatRoom {
     addUser (user: ChatUser) {
         this.users[user.name] = user;
     }
+    getUserByName (userName: string): ChatUser {
+        for(var key in this.users) {
+            if(this.users[key] && this.users[key].name == userName) {
+                return this.users[key];
+            }
+        }
+        return null;
+    }
     addMessage (userName: string, message: string): void {
         if(!this.users[userName]) throw "Invalid User";
         this.users[userName].messages.push(message);
